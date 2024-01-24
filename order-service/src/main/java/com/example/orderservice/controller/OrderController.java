05199bf5c56d25024ca4a7ebd44707339d0df6c7
PavelM2205 <pavelm2205@mail.ru>
2024-01-24 10:21:53 +0700

    Add API Gateway with Spring Cloud Gateway

--- order-service/src/main/java/com/example/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/example/orderservice/controller/OrderController.java
@@ -5,14 +5,12 @@ import com.example.orderservice.dto.OrderCreateResponseDto;
 import com.example.orderservice.dto.OrderReadDto;
 import com.example.orderservice.service.OrderService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController("/orders")
+@RestController
+@RequestMapping("/orders")
 @RequiredArgsConstructor
 public class OrderController {
     private final OrderService orderService;
